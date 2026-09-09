c0db442a3c0af218cc745124b861c1c93f69f843
강언덕 <64720058+123456789qwaszx@users.noreply.github.com>
2026-09-09 15:21:52 +0900

    feat(rewrite-m1): Game 클래스 작성

---
+++ src/main/java/com/gamebasic/game/entity/Game.java
@@ -0,0 +1,42 @@
+﻿package com.gamebasic.game.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "games")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 12)
+    private String playerName;
+
+    @Column(nullable = false)
+    private int currentHp;
+
+    @Column(nullable = false)
+    private int currentFloor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 16)
+    private GamePhase phase;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 16)
+    private GameStatus status;
+
+    public Game(String playerName){
+        this.playerName = playerName;
+        this.currentHp = 99;
+        this.currentFloor = 1;
+        this.phase = GamePhase.REWARD;
+        this.status = GameStatus.PLAYING;
+    }
+}
---
+++ src/main/java/com/gamebasic/game/entity/GamePhase.java
@@ -0,0 +1,8 @@
+﻿package com.gamebasic.game.entity;
+
+// 지금 어느 진행 단계(화면) 인지
+public enum GamePhase {
+    REWARD,
+    BATTLE,
+    FINISHED
+}
